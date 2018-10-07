78d465b72969a5a0463c22f97ff2b779923924fc
Jacob Zavatone-Veth <jacob.zavatone-veth@yale.edu>
2018-10-07 17:10:14 -0400

    Initial source commit

---
+++ HHGPermutationTest.m
@@ -0,0 +1,291 @@
+function [p, t, pstat] = HHGPermutationTest(X,Y,nrep,maxN)
+%HHGPermutationTest Heller-Heller-Gorfine multivariate test of association
+%   
+%   Syntaxes:
+%       [ p ] = HHGPermutationTest(X,Y) computes the p-value of the HHG
+%           test using 100 permutations for an (Nxm1) matrix X and an
+%           (Nxm2) matrix Y.
+%
+%       [ p ] = HHGPermutationTest(X,Y,nrep) computes the p-value of the
+%           HHG test using nrep permutations
+%
+%       [ p ] = HHGPermutationTest(X,Y,nrep,maxN) computes the p-value of
+%           the HHG test while storing at most a (maxN x maxN) distance
+%           matrix in memory. If n > maxN, out-of-core computation is used.
+%
+%       [p, t] = HHGPermutationTest(...);
+%       [p, t] = HHGPermutationTest(...);
+%   
+%   Inputs:
+%       'X', 'Y' - Input data matrices. Rows are assumed to represent
+%           samples, and columns are assumed to represent dimensions.
+% 
+%       'nrep' - The number of permutations to use in computing the exact
+%           test. The default value is 100.
+%
+%       'maxN' - The maximum number of input samples for which the entire
+%           distance matrix will be stored in memory. The default value is
+%           1e4.
+%       
+%   Outputs:
+%       'p' - The p-value of the permutation test.
+%
+%       't' - The HHG test statistic.
+%
+%       'pstat' - A nrep by 1 vector of the HHG test statistics for each
+%           permutation.
+%
+%   References:
+%       [1] - Heller, R., Heller, Y., & Gorfine, M. (2012). A consistent
+%           multivariate test of association based on ranks of distances.
+%           Biometrika, 100(2), 503-510. 
+%
+
+% Set the  number of permutations
+if (nargin < 3) || isempty(nrep)
+    nrep = 100;
+elseif (~isscalar(nrep)) || (rem(nrep,1) > 0) || (nrep < 1)
+    error('The number of permutations must be a scalar integer greater than or equal to 1.');
+end
+
+% Set the maximum number points for which the full NxN distance matrix will
+% be held in memory
+if (nargin < 4) || isempty(maxN)
+    maxN = 1e4;
+elseif (~isscalar(maxN)) || (rem(maxN,1) > 0) || (maxN < 1)
+    error('The maximum number of points for in-memory computation must be a scalar integer greater than or equal to 1.');
+end
+
+% Get the number of samples
+n = size(X,1);
+
+% Get the field width of the number of permutations
+ndig = 1+floor(log10(nrep));
+
+% Allocate a container to store the values of the test statistic for each permutation
+pstat = nan(nrep,1);
+
+% Start a timer
+tAll = tic;
+
+% Decide whether to store the full pairwise distance matrices in memory
+if n < maxN
+    
+    % Compute the full distance matrices
+    fprintf('%d points: computing full pairwise distance matrices.\n', n);
+    dx = squareform(pdist(X));
+    dy = squareform(pdist(Y));
+    fprintf('Computed distance matrices: %f seconds elapsed.\n', toc(tAll));
+    
+    % Compute the test statistic
+    t = hhgTestStatisticFromFullDistanceMatrices(dx,dy);
+    fprintf('Computed test statistic: %f seconds elapsed.\n', toc(tAll));
+    
+    % Compute the distribution of the test statistic under permutation of y
+    for ind = 1:nrep
+        idx = randperm(n);
+        pstat(ind) = hhgTestStatisticFromFullDistanceMatrices(dx, dy(idx,idx));
+        fprintf('Permutation %*d of %d: %f seconds elapsed.\n', ndig, ind, nrep, toc(tAll));
+    end
+    
+else
+    
+    % If we fall into this case, the distance matrix is too large, and we
+    % must compute the test statistic incrementally.
+    fprintf('%d points: using out-of-core algorithm.\n', n);
+    
+    % Incrementally compute distance matrices and write them to files
+    [ posx, posy ] = hhgDistanceMatrixToFile(X,Y);
+    fprintf('Computed distance matrices: %f seconds elapsed.\n', toc(tAll));
+    
+    % Compute the test statistic
+    t = hhgTestStatisticIncremental(posx,posy,(1:n)');
+    fprintf('Computed test statistic: %f seconds elapsed.\n', toc(tAll));
+    
+    % Compute the distribution of the test statistic under permutation of y
+    for ind = 1:nrep
+        idx = randperm(n);
+        pstat(ind) = hhgTestStatisticIncremental(posx,posy,idx);
+        fprintf('Permutation %*d of %d: %f seconds elapsed.\n', ndig, ind, nrep, toc(tAll));
+    end
+    
+    % Delete temporary files
+    delete('dx.dat');
+    delete('dy.dat');
+    
+end
+
+% Compute the p-value
+p = nnz(pstat >= t) / nrep;
+
+% Print a timing message to the terminal
+fprintf('Computation finished in %f seconds\n', toc(tAll));
+
+end
+
+function [ t ] = hhgTestStatisticFromFullDistanceMatrices(dx,dy)
+% Local utility function to compute the HHG test statistic from precomputed
+% distance matrices
+
+% Get the number of samples
+n = size(dx,1);
+m = n - 1;
+
+% Compute needed indexing vectors
+c = (1:n)';
+j = (1:m)';
+
+% Initialize the test statistic
+t = 0;
+
+% Iterate over samples
+for i = 1:n
+    % Extract the desired data from the distance matrices
+    dx_tmp = dx(c~=i, i);
+    dy_tmp = dy(c~=i, i);
+    
+    % Re-order the distances by the distance to i in x
+    [dx_tmp, jIdx] = sort(dx_tmp, 'ascend');
+    dy_tmp = dy_tmp(jIdx);
+    
+    % Compute the permutation pi(j) defined as the rank of the distances to i in y
+    [~,pij] = ismember(dy_tmp, sort(dy_tmp, 'ascend'));
+    
+    % Count the number of inversions for each j
+    invj = double(inversions(uint64(pij)));
+    
+    % Compute the contingency table
+    a11 = j - 1 - invj;
+    a12 = invj;
+    a21 = pij + invj - j;
+    a22 = n - 1 - pij - invj;
+    
+    % Compute the classical test statistic
+    snum = (a12 .* a21 - a11 .* a22).^2;
+    sden = (j-1).*(n-j-1).*(pij-1).*(n-1-pij);
+    s = snum ./ sden;
+    s(sden==0) = 0;
+    
+    % Accumulate the test statistic
+    t = t + sum(s);
+end
+
+% Adjust the test statistic
+t = (n-2)*t;
+
+end
+
+function [ posx, posy ] = hhgDistanceMatrixToFile(x,y)
+% Incrementally compute distance matrices and write them to disk
+
+% If distance matrix files already exist, delete them
+if (exist('dx.dat', 'file') == 2), delete('dx.dat'); end
+if (exist('dy.dat', 'file') == 2), delete('dy.dat'); end
+
+% Open file IDs
+fx = fopen('dx.dat','w');
+fy = fopen('dy.dat','w');
+
+% Get the number of samples
+n = size(x,1);
+
+% Allocate containers to store offsets
+posx = nan(n,1);
+posy = nan(n,1);
+
+% Iterate over samples
+for i = 1:n
+    % Get current offsets
+    posx(i) = ftell(fx);
+    posy(i) = ftell(fy);
+    
+    % Compute distances in x and y from point i
+    dx = pdist2(x, x(i,:));
+    dy = pdist2(y, y(i,:));
+    
+    % Write distances to files
+    fwrite(fx, dx, 'double');
+    fwrite(fy, dy, 'double');
+    
+end
+
+% Close file IDs
+fclose(fx);
+fclose(fy);
+
+end
+
+
+function [ t ] = hhgTestStatisticIncremental(posx, posy, idx)
+% Local utility function to compute the HHG test statistic incrementally
+% using an out-of-core algorithm
+
+% Get the number of samples
+n = size(posx,1);
+m = n - 1;
+
+% Compute needed indexing vectors
+c = (1:n)';
+j = (1:m)';
+
+% Initialize the test statistic
+t = 0;
+
+% Open file IDs
+fx = fopen('dx.dat','r');
+fy = fopen('dy.dat','r');
+
+% Permute posy
+posy = posy(idx);
+
+% Iterate over samples
+for i = 1:n
+    % Seek to the needed locations in the distance matrix files
+    fseek(fx, posx(i), 'bof');
+    fseek(fy, posy(i), 'bof');
+    
+    % Read in distance matrix data from file
+    dx = fread(fx, n, 'double');
+    dy = fread(fy, n, 'double');
+    
+    % Permute y-distances
+    dy = dy(idx);
+    
+    % Remove the current point
+    dx = dx(c~=i);
+    dy = dy(c~=i);
+    
+    % Re-order the distances by the distance to i in x
+    [dx, jIdx] = sort(dx, 'ascend');
+    dy = dy(jIdx);
+    
+    % Compute the permutation pi(j) defined as the rank of the distances to i in y
+    [~,pij] = ismember(dy, sort(dy, 'ascend'));
+    
+    % Count the number of inversions for each j
+    invj = double(inversions(uint64(pij)));
+    
+    % Compute the contingency table
+    a11 = j - 1 - invj;
+    a12 = invj;
+    a21 = pij + invj - j;
+    a22 = n - 1 - pij - invj;
+    
+    % Compute the classical test statistic
+    snum = (a12 .* a21 - a11 .* a22).^2;
+    sden = (j-1).*(n-j-1).*(pij-1).*(n-1-pij);
+    s = snum ./ sden;
+    s(sden==0) = 0;
+    
+    % Accumulate the test statistic
+    t = t + sum(s);
+end
+
+% Close file IDs
+fclose(fx);
+fclose(fy);
+
+% Adjust the test statistic appropriately
+t = (n-2)*t;
+
+end
---
+++ inversions.c
@@ -0,0 +1,101 @@
+/*==========================================================
+ * inversions.c
+ *
+ * Computes the inversion vector of a permutation
+ *
+ * The calling syntax is:
+ *
+ *		[ invs ] = inversions(p)
+ *
+ * This is a MEX-file for MATLAB.
+ *
+ *========================================================*/
+
+#include "mex.h"
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdint.h>
+
+/*
+O(n*log(n)) inversion counting without merge sort, thanks to
+https://stackoverflow.com/questions/48977484/inversion-count-algorithm-without-using-merge-sort-c
+*/
+void Inversions(uint64_t *a, uint64_t *b, uint64_t dim)
+{
+  /* Initialize a counter of the number of zero values in the array a */
+  uint64_t z = 0;
+
+  /* Allocate a temporary container */
+  size_t n = (dim+2) * sizeof(uint64_t);
+  uint64_t *c = (uint64_t*) malloc(n);
+
+  /* Iterate until all values of a are zero */
+  do {
+    /* Set all elements of c to zero at each iteration */
+    memset(c, 0, n);
+
+    /* Iterate over elements */
+    for (int i = 0; i < dim; i++) {
+      if (a[i] % 2 == 0) {
+        b[i] += c[a[i]+1];
+      } else{
+        c[a[i]] += 1;
+      }
+    }
+
+    z = 0; /* Reset the zero counter */
+
+    /* Iterate over elements */
+    for (int k = 0; k < dim; k++){
+      /* Divide each element of a by 2 (as integers) */
+      a[k] /= 2;
+      /* Accumulate the counter of zeros */
+      if (a[k] == 0) { z++; }
+    }
+  } while (z != dim);
+
+  free(c); /* Free the temporary container */
+}
+
+/* The gateway function */
+void mexFunction( int nlhs, mxArray *plhs[],
+                  int nrhs, const mxArray *prhs[])
+{
+  uint64_t *source;
+  uint64_t *permutation;
+  uint64_t dim;
+  uint64_t *inversion_count;
+
+  /* Validate the number of inputs and outputs */
+  if (nrhs != 1) {mexErrMsgIdAndTxt("inversions:nrhs", "One input required.");}
+  if (nlhs != 1) {mexErrMsgIdAndTxt("inversions:nlhs", "One output required.");}
+
+  /* Validate the types of the inputs */
+  if (!mxIsUint64(prhs[0])) {
+    mexErrMsgIdAndTxt("nversions:intype", "Input must be of type uint64.");
+  }
+
+  /* Validate the dimensions of the inputs */
+  if (mxGetN(prhs[0]) != 1) {
+    mexErrMsgIdAndTxt("inversions:incolumn", "Input must be a column vector.");
+  }
+  if (mxGetM(prhs[0]) <= 1) {
+    mexErrMsgIdAndTxt("inversions:inshape", "Input must have at least two elements.");
+  }
+
+  /* Get a pointer to a deep copy of the data in the input matrix */
+  permutation = (uint64_t*) mxGetData(mxDuplicateArray(prhs[0]));
+
+  /* Get the dimensions of the input vectors */
+  dim = (uint64_t) mxGetM(prhs[0]);
+
+  /* Create the output matrix */
+  plhs[0] = mxCreateNumericMatrix((mwSize)dim, 1, mxUINT64_CLASS, mxREAL);
+
+  /* Get a pointer to the output matrix */
+  inversion_count = (uint64_t*) mxGetData(plhs[0]);
+
+  /* Call the computational routine */
+  Inversions(permutation, inversion_count, dim);
+}
